86ac54c8f0dc8a6242b39b9438218f0ba414f4d7
Rabby <fazlay_rabby@hotmail.com>
2020-04-06 23:32:18 -0700

    cleaned

--- app/src/main/java/com/example/deviceregistration/models/SendJSON.java
+++
@@ -1,9 +0,0 @@
-package com.example.deviceregistration.models;
-
-public class SendJSON {
-    public String username;
-    public String password;
-    public String SA;
-    public String MA;
-
-}
